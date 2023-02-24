49f0d6974db1b0950d74e9d0151ed193ac078aa6
AqeelHashmi01 <aqeelhashmi26@gmail.com>
2023-02-24 11:51:21 +0500

    screenshots added

--- App.js
+++ App.js
@@ -2,16 +2,12 @@ import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
 
 export default function App() {
-  // Declare state variables for city and weather data
   const [city, setCity] = useState('');
   const [weather, setWeather] = useState(null);
   const [shouldFetchWeather, setShouldFetchWeather] = useState(false);
 
-  // Use useEffect hook to fetch weather data when city changes
   useEffect(() => {
-    // Only fetch data if city is not empty
     if (city !== '') {
-      // Fetch weather data from OpenWeatherMap API
       fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=06d9447e0f4f4be54d99f7e956eee080&units=metric`)
         .then(response => response.json())
         .then(data => setWeather(data))
@@ -20,33 +16,25 @@ export default function App() {
     setShouldFetchWeather(false);
   }, [shouldFetchWeather]);
 
-  // Handle button press to clear weather data
   const handlePress = () => {
-    // setWeather(null);
     setShouldFetchWeather(true);
   };
 
   return (
     <View style={styles.container}>
-      {/* Only render weather data if it exists */}
       {weather && weather.weather && (
         <View style={styles.weather}>
-          {/* Display city name */}
           <Text style={styles.title}>{weather.name}</Text>
-          {/* Display weather description */}
           <Text style={styles.subtitle}>{weather.weather[0].description}</Text>
-          {/* Display temperature */}
           <Text style={styles.temp}>{Math.round(weather.main.temp)}&deg;C</Text>
         </View>
       )}
-      {/* Input for user to enter city name */}
       <TextInput
         style={styles.input}
         placeholder="Enter city name"
         value={city}
         onChangeText={text => setCity(text)}
       />
-      {/* Button to fetch weather data */}
       <TouchableOpacity style={styles.button} onPress={handlePress}>
         <Text style={styles.buttonText}>Get Weather</Text>
       </TouchableOpacity>
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# weather-app
+# Weather-app
 react native - weather application
+
+# Screenshots of App running on android device:
+![Weather app screenshot 1](./demo/weatherApp_ss1.jpg)
+![Weather app screenshot 1](./demo/weatherApp_ss2.jpg)
+
